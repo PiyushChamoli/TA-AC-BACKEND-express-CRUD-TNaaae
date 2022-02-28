31db8300f9b853e1f258d1ea2fc27052a91cf381
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-02-28 22:29:57 +0530

    add file

--- block-BNaacu/school/app.js
+++ block-BNaacu/school/app.js
@@ -5,13 +5,22 @@ var studentsRouter = require('./routes/students')
 var app = express()
 
 // middleware
+app.use(express.urlencoded({extended:false}))
 
 // setup view engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
 //routing middleware
+app.get('/', (req,res) => {
+    res.send('Home Page')
+})
 app.use('/students', studentsRouter)
+
+// error handle
+app.use((req,res) => {
+    res.send('PAGE NOT FOUND')
+})
 
 app.listen(3000, () => {
     console.log('server listening on port 3000')
