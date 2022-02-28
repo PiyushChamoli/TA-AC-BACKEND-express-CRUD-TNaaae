01993c0a1d1a081c2c960a7ba237f849b99bf723
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-02-28 23:47:50 +0530

    add

--- block-BNaacv/user-diary/app.js
+++ block-BNaacv/user-diary/app.js
@@ -1,21 +1,24 @@
 var express = require('express')
-var userRoute = require('./routes/user')
+var path = require('path')
+var userRoute = require('./routes/userRoute')
 
 var app = express()
 
 // middleware
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.set('views', path.join(__dirname, '/views'))
+
+app.use(express.urlencoded({extended:false}))
 
 // routing middleware
 app.get('/', (req,res) => {
     res.send('HOME PAGE')
 })
-app.get('/users', userRoute)
+app.use('/users', userRoute)
 
 // error handle
 app.use((req,res) => {
-    res.send('PAGE NOT FOUND')
+    res.status(404).send('PAGE NOT FOUND')
 })
 
 app.listen(3000, () => {
